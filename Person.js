825512e11b060e67199a479c84e157a663853611
Steven Hall <stevenhall@scottlogic.co.uk>
2015-01-22 10:57:13 +0000

    Fixed it!

--- Person.js
+++ Person.js
@@ -2,24 +2,30 @@
     "use strict";
     
     function Person(spec) {
-        var firstName = spec.firstName,
-            lastName = spec.lastName,
-            getName = function() {
-                return firstName + " " + lastName;
-            };
+        var firstName = spec.firstName;
+        var lastName = spec.lastName;
         
-            return Object.freeze({
-                getName: getName
-            });
+        return Object.freeze({
+            getName: function() {
+                return firstName + " " + lastName;
+            }
+        });
     };
-    
-    var james_bond = new Person("James", "Bond");
+        
+    Person.prototype.getFullName = function() {
+        return "no visibility";
+    }
+
+    var james_bond = new Person({ firstName: "James", lastName: "Bond"});
     console.log(james_bond.firstName);
     console.log(james_bond.getName());
-    
+
     james_bond.getName = function() {
         return "Jimmy Bond";
     };
+
+    console.log(james_bond.getName());
+    console.log(james_bond.getFullName());
     
 })();
 
